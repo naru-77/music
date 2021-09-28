ca259a9433eed3cef24cc12d10dfab6d390f15f5
naru-77 <naru.fumi@icloud.com>
2021-09-29 01:19:47 +0900

    Initial Commit

---
+++ Shared/mymusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  mymusicApp.swift
+//  Shared
+//
+//  Created by ナルセ　ユウキ on 2021/09/29.
+//
+
+import SwiftUI
+
+@main
+struct mymusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
